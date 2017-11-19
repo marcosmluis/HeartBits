f2595a58c755579e62656c461ad9e8af3914a692
josesantos67687 <josesantos.11@hotmail.com>
2017-11-19 04:57:57 +0000

    static records added

--- app/src/main/java/com/example/jose/heartbits/Registos.java
+++ app/src/main/java/com/example/jose/heartbits/Registos.java
@@ -6,6 +6,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by jose on 19-11-2017.
@@ -13,14 +18,27 @@ import android.widget.Button;
 
 public class Registos extends AppCompatActivity {
 
-    ArrayAdapter<String> adapter;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registos);
+        String[] fruits = new String[] {
+                "Cape Gooseberry",
+                "Capuli cherry"
+        };
+        List<String> list = new ArrayList<String>(Arrays.asList(fruits));
 
-        adapter.add("New Item");
-        adapter.add("New Item");
+        ListView lv = (ListView) findViewById(R.id.listaregistos);
+
+
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>
+                (this, android.R.layout.simple_list_item_1, list);
+
+        lv.setAdapter(arrayAdapter);
     }
 }
